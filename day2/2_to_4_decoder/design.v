c3bd7a8235bcd6ade86f8c6f386ddbd82c1ca9f1
SREEJITH.S <sreejithkollam2006@gmail.com>
2026-06-09 21:55:55 +0530

    Update design.v

--- day2/2_to_4_decoder/design.v
+++ day2/2_to_4_decoder/design.v
@@ -1,1 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 16:07:38
+// Design Name: 
+// Module Name: Decoder_2_4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Decoder_2_4(input [1:0]i, output reg [3:0]Y);
+always @(*)
+begin
+case(i)
+2'b00:Y=4'b0001;
+2'b01:Y=4'b0010;
+2'b10:Y=4'b0100;
+2'b11:Y=4'b1000;
+default:Y=4'b0000;
+
+endcase
+end
+endmodule
 
