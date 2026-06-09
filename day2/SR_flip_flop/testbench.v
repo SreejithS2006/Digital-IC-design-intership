0f86b8dd7f54193cc348abdd4bfb786f00184b96
SREEJITH.S <sreejithkollam2006@gmail.com>
2026-06-09 22:02:06 +0530

    Update testbench.v

--- day2/SR_flip_flop/testbench.v
+++ day2/SR_flip_flop/testbench.v
@@ -1,1 +1,61 @@
+module sr_flipflop_tb();
+reg s_tb;
+reg r_tb;
+reg rst_tb;
+reg clk_tb;
+wire q_tb;
+wire qbar_tb;
 
+sr_flipflop dut (
+    .s(s_tb),
+    .r(r_tb),
+    .rst(rst_tb),
+    .clk(clk_tb),
+    .q(q_tb),
+    .qbar(qbar_tb)
+);
+
+initial
+begin
+    clk_tb = 0;
+    always #5 clk_tb = ~clk_tb;
+end
+
+initial
+begin
+    s_tb = 0;
+    r_tb = 0;
+    rst_tb = 1;
+
+    #10;
+    rst_tb = 0;
+
+    s_tb = 1;
+    r_tb = 0;
+    #10;
+
+    s_tb = 0;
+    r_tb = 0;
+    #10;
+
+    s_tb = 0;
+    r_tb = 1;
+    #10;
+
+    s_tb = 0;
+    r_tb = 0;
+    #10;
+
+    s_tb = 1;
+    r_tb = 1;
+    #10;
+
+    $finish;
+end
+
+initial
+begin
+    $monitor(" clk=%b rst=%b s=%b r=%b q=%b qbar=%b",
+              clk_tb, rst_tb, s_tb, r_tb, q_tb, qbar_tb);
+end
+endmodule
