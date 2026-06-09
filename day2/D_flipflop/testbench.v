709e2192402c7778d1cf8f71473d7e2920403ede
SREEJITH.S <sreejithkollam2006@gmail.com>
2026-06-09 22:07:09 +0530

    Update testbench.v

--- day2/D_flipflop/testbench.v
+++ day2/D_flipflop/testbench.v
@@ -1,1 +1,33 @@
+module DFF_tb();
+reg d_tb;
+reg rst_tb;
+reg clk_tb;
+wire q_tb;
+wire qbar_tb;
+DFF dut( d_tb,rst_tb,clk_tb,q_tb,qbar_tb);
+initial
+begin
+    clk_tb = 0;
+    forever #5 clk_tb = ~clk_tb;
+    end
+    initial
+begin
+    d_tb=0;
+    rst_tb = 1;
 
+    #10;
+    rst_tb = 0;
+
+    d_tb = 0;
+    #10;
+
+    d_tb = 1;
+   
+end
+
+initial
+begin
+    $monitor("Time=%0t clk=%b rst=%b d=%b  q=%b qbar=%b",
+             $time, clk_tb, rst_tb, d_tb, q_tb, qbar_tb);
+end
+endmodule
